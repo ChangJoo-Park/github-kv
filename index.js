4bc7e3d447674f6344b6c0fad3e26b1fb0cf89a4
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-08-07 21:51:46 +0900

    add meow for help

--- index.js
+++ index.js
@@ -3,17 +3,34 @@
 const fs = require('fs')
 const axios = require('axios')
 const minimist = require('minimist')
+const meow = require('meow')
 
-const args = minimist(process.argv.slice(2), {
+const minimistOptions = {
     string: ['token', 'owner', 'repo', 'file', 'extract'],
     alias: { t: 'token', o: 'owner', r: 'repo', f: 'file', e: 'extract' }
-})
+}
+
+const args = minimist(process.argv.slice(2), minimistOptions)
 
 const { token, owner, repo, file, extract } = args
 
 if (!token || !owner || !repo || !file) {
-    console.log('PLEASE CHECK arguments')
-    return
+    console.log('Need help? `gh-kv --help`')
+
+    return meow(`
+    Usage:
+        gh-kv [options]
+
+        gh-kv.cmd --token <GitHub Token> --owner <Owner or Organization> --repo <REPO_NAME> --file <TARGET_FILENAME> --extract <OUTPUT_FILENAME>
+
+    Options:
+        -h, --help         print usage information
+        -t, --token        access token for Github
+        -o, --owner        target repository owner or organization
+        -r, --repo         target repository name
+        -f, --file         target file for fetch
+        -e, --extract      file name for result eg. key.json [not required]`,
+        minimistOptions)
 }
 
 const url = `https://api.github.com/repos/${owner}/${repo}/contents/${file}`
