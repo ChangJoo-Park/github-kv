d20ed0c907ba363a5ea0a348f67574e1a6a81e64
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-08-07 21:14:43 +0900

    Complete common scenario

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const fs = require('fs')
+const axios = require('axios')
+const minimist = require('minimist')
+
+const args = minimist(process.argv.slice(2), {
+    string: ['token', 'owner', 'repo', 'file', 'extract'],
+    alias: { t: 'token', o: 'owner', r: 'repo', f: 'file', e: 'extract' }
+})
+const { token, owner, repo, file, extract } = args
+
+if (!token || !owner || !repo || !file) {
+    console.log('PLEASE CHECK arguments')
+    return
+}
+
+axios({
+    headers: {
+        Authorization: `token ${token}`,
+        Accept: 'application/vnd.github.v3.raw',
+    },
+    url: `https://api.github.com/repos/${owner}/${repo}/contents/${file}`
+})
+    .then(response => {
+        console.log(response.data)
+        if (extract) {
+            fs.writeFileSync(extract, JSON.stringify(response.data))
+        }
+    })
+    .catch(error => {
+        console.error(error)
+    })
