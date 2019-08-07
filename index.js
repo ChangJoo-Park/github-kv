f27c73208025834f350d22529f89635a2f340af9
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-08-07 21:38:37 +0900

    formatting index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const args = minimist(process.argv.slice(2), {
     string: ['token', 'owner', 'repo', 'file', 'extract'],
     alias: { t: 'token', o: 'owner', r: 'repo', f: 'file', e: 'extract' }
 })
+
 const { token, owner, repo, file, extract } = args
 
 if (!token || !owner || !repo || !file) {
@@ -16,19 +17,17 @@ if (!token || !owner || !repo || !file) {
     return
 }
 
-axios({
-    headers: {
-        Authorization: `token ${token}`,
-        Accept: 'application/vnd.github.v3.raw',
-    },
-    url: `https://api.github.com/repos/${owner}/${repo}/contents/${file}`
-})
+const url = `https://api.github.com/repos/${owner}/${repo}/contents/${file}`
+const headers = {
+    Authorization: `token ${token}`,
+    Accept: 'application/vnd.github.v3.raw',
+}
+axios({ headers, url })
     .then(response => {
-        console.log(response.data)
         if (extract) {
             fs.writeFileSync(extract, JSON.stringify(response.data))
         }
     })
     .catch(error => {
-        console.error(error)
+        console.error(error.response)
     })
